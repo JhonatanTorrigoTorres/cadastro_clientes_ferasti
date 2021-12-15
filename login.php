e37770a40a2c6f50a9d7b182a0da6a56869f056c
Jhonatan Torrigo Torres <jhonatanttorres@gmail.com>
2021-12-15 11:17:26 -0300

    teste

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tela de Login</title>
+    </head>
+    <body>
+        <section>
+            <div>
+                <img src="./img/logo_horizonta_fundo_branco.png" alt="imagem">
+            </div>
+            <div>
+                <p>Bem-vindo</p>
+            </div>
+            <form action="autenticar.php" method="post">
+                <label>E-mail</label>
+                <input type="email" name="email" >
+            </form>
+        </section>
+        
+    </body>
+</html>
