d7a1d5b1b7430303725939fc5b730dd61f558154
Jhonatan Torrigo Torres <jhonatanttorres@gmail.com>
2021-12-15 14:04:04 -0300

    salvando

--- login.php
+++ login.php
@@ -4,21 +4,21 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/login.css">
+        <script src="./js/login.js"></script>
         <title>Tela de Login</title>
     </head>
     <body>
-        <section>
-            <div>
-                <img src="./img/logo_horizonta_fundo_branco.png" alt="imagem">
-            </div>
-            <div>
-                <p>Bem-vindo</p>
-            </div>
+        <main>
+            <img src="./img/logo_horizonta_fundo_branco.png" alt="imagem">
+            <p><b>Bem-vindo</b></p>
             <form action="autenticar.php" method="post">
                 <label>E-mail</label>
-                <input type="email" name="email" >
+                <input type="email" name="email" id="email" placeholder="Insira seu login" />
+                <label>Senha</label>
+                <input type="password" name="senha" id="senha" placeholder="Insira sua senha" />
+                <input type="submit" value="Login" id="login" onclick>
             </form>
-        </section>
-        
+        </main>
     </body>
 </html>
