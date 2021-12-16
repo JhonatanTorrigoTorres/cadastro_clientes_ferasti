5383c9fcc1e131993f708f40635dc69790739a10
Jhonatan Torrigo Torres <jhonatanttorres@gmail.com>
2021-12-16 17:29:43 -0300

    salvando

---
+++ clientes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="./css/clientes.css">
+        <title>Clientes</title>
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="./img/logo_horizonta_fundo_branco.png" alt="logotipo">
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Jhonatan Torres
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Sair</a></li>
+                </ul>
+            </div>
+        </header>
+    </body>
+</html>
