0a707b8796481f8bbd6b8b667a24ddadaeb4824a
Jhonatan Torrigo Torres <jhonatanttorres@gmail.com>
2021-12-24 17:55:50 -0300

    Máscaras Jquery
    Foi trocado o plugin por uma URl

--- alterar.php
+++ alterar.php
@@ -17,7 +17,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <script src="./js/jQuery-Mask-Plugin-master/src/jquery.mask.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.11.2/jquery.mask.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.11.2/jquery.mask.js"></script>
         <link rel="stylesheet" href="./css/alterar.css">
         <title>Alterar</title>
     </head>
--- cadastrar.php
+++ cadastrar.php
@@ -28,7 +28,8 @@ if (
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <script src="./js/cadastrar.js"></script>
-        <script src="./js/jQuery-Mask-Plugin-master/src/jquery.mask.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.11.2/jquery.mask.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.11.2/jquery.mask.js"></script>
         <link rel="stylesheet" href="./css/alterar.css">
         <title>Cadastrar</title>
     </head>
